3985c05a9cf5ffcf4be4f87676c4aa165b03156a
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-27 03:10:29 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,73 @@
-# your code goes here
+def begins_with_r(arr)
+  count = 0
+  arr.each do |ele|
+    if ele[0] == "r"
+      count += 1
+    end
+  end
+  count == arr.size ? true : false
+    
+end
+
+def contain_a(arr)
+  my_arr = []
+  arr.each do |ele|
+    if ele.include?("a")
+      my_arr << ele
+    end
+  end
+  my_arr
+end
+
+def first_wa(arr)
+  arr.each do |ele|
+    if ele.to_s == ele
+      if ele.include?("wa") 
+        return ele
+      end
+    end
+  end
+  return nil
+end
+
+def remove_non_strings(arr)
+  my_arr = []
+  arr.map do |ele|
+    if ele.is_a? String
+      my_arr << ele
+    end
+  end
+  my_arr
+end
+
+def count_elements(arr)
+  arr.each do |ele|
+    ele[:count] = 0
+    name = ele[:name]
+    arr.each do |ele2|
+      if ele2[:name] == name
+        ele[:count] += 1
+      end
+    end
+  end.uniq
+end
+
+def merge_data(keys, data)
+  data_hash = {}
+  keys.each do |key|
+    data.each do |ele|
+      if key[:first_name] == ele.key
+  
+end
+
+def find_cool(cool)
+  
+end
+
+def organize_schools(schools)
+  
+end
+
+
+
+
